89c73041ab1aed9dd7c7158a381a8fcfa373da53
krv006 <rvkamronbek@gmail.com>
2025-08-06 19:25:09 +0500

    add

---
+++ Tasks/main1.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+/*
+     vector<int> juft;
+    vector<int> toq;
+
+    for (int i = 0; i <= 100; i++) {
+        if (i % 2 == 0) {
+            juft.push_back(i);
+        } else toq.push_back(i);
+
+    }
+
+    for (int son: juft) {
+        cout << son << ", ";
+    }
+    cout << "JUFT SON" << endl;
+    for (int son: toq) {
+        cout << son << ", ";
+    }
+    cout << "TOQ SON" << endl;
+
+ */
+
+/*
+     int s = 0;
+    for (int i = 0; i <= 6; i++) {
+        if (i % 2 == 1) {
+            s += i;
+        }
+    }
+
+    cout << "Toq sonlar yigindisi: " << s;
+
+ */
+
+
+/*
+     string s = "asal";
+    int count = 0;
+
+    for(int i=0;i < s.length(); i++){
+        if(s[i] == 'a'){
+            count++;
+        }
+    }
+    cout << count << endl;
+
+ */
+
+/*
+     int n;
+    cout << "A: ";
+    cin >> n;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            cout << "Tub son emas." << endl;
+            return 0;
+        }
+    }
+
+    cout << "Tub son." << endl;
+
+ */
+
+/*
+
+    string s = "Kamron";
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        cout << s[i];
+    }
+
+ */
+
+    
+
+
+
+
+
+
+
+    return 0;
+
+}
